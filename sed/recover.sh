56c3277a5ed7516c5b28a1d6c9c77b556f12c9ed
Kim In Hoi <31230862+hotire@users.noreply.github.com>
2020-12-11 23:27:33 +0900

    Add recover.sh

---
+++ sed/recover.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ex) target text abc : 123, please##
+
+result="$(
+grep 'target text' fileName* | 
+sed "s/^.*target text, //" |
+sed "s/, please.*$//" |
+sed 's/##//g' |
+sed 's/abc :/type/g')"
+
+api="http://localhost:8080/api"
+
+echo $result
+
+str=$api
+count=0
+
+
+for item in $result
+do
+
+    count=$(( ${count}+1 ))
+
+    str=$str'/'$item
+
+    rem=`expr $count % 2`
+
+        if [ $rem -eq 0 ]
+        then
+                echo $str
+                curl -X GET $str
+                str=$api
+        fi
+
+done
